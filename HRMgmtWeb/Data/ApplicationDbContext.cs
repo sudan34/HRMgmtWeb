367d5c94ed36b2cf8b512a851b285bf477d6e3cd
Madhu sudan Bhattarai <summerpalpali@gmail.com>
2025-05-04 06:17:24 +0545

    added dapper

--- HRMgmtWeb/Data/ApplicationDbContext.cs
+++ HRMgmtWeb/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;  
+﻿using HRMgmtWeb.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;  
 using Microsoft.EntityFrameworkCore;
 
 namespace HRMgmtWeb.Data
